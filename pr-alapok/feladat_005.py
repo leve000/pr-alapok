d19a18bbfa2e2a286dc7cc55377b54e6aad0c037
Gyurcsaklj <113088468+leve000@users.noreply.github.com>
2023-10-06 13:04:46 +0200

    Add files via upload

---
+++ pr-alapok/feladat_005.py
@@ -0,0 +1,10 @@
+
+"""
+Amikor karakterlánccá alakítunk, az str utasításra van szükség.
+"""
+felhasználó_kora = 16
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+felhasználó_kora = str(felhasználó_kora)
+print(f"A felhasználó_kora változó típusa: {type(felhasználó_kora)}")
+ilyen = input('És milyen ' + felhasználó_kora + ' évesnek lenni? ')
+olyan = input(f'És milyen {felhasználó_kora} évesnek lenni? ')
